0e30915ab7edfa76e0fc08e8b653ddb3a0d1eca5
Steven Tseng <booda406@gmail.com>
2014-01-07 18:39:04 +0800

    modify application

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'sass-rails', '~> 4.0.0'
 
 gem 'omniauth'
 gem 'omniauth-facebook'
+gem 'bootstrap-datepicker-rails'
+gem 'simple_form'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107095520) do
+ActiveRecord::Schema.define(version: 20140107101337) do
 
   create_table "events", force: true do |t|
     t.boolean  "follow"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140107095520) do
     t.string   "item"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
