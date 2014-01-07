789716515fc89b1e782bbd8b0b4cec01aaf575f9
Steven Tseng <booda406@gmail.com>
2014-01-07 18:06:12 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Gamble::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => 'users/omniauth_callbacks'
+  }
+
+
   resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
